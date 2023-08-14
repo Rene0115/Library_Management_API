52c721ffe2cf93c3b6f59804b51ddb558f656862
Rene0115 <enere0115@gmail.com>
2023-08-14 16:03:43 +0100

    added login route

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import userController from '../controllers/user.controller.js';
+import express from "express";
+import userController from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/signup', userController.signup);
+userRouter.post("/signup", userController.signup);
+userRouter.post("/login", userController.login);
 
-export default userRouter
+export default userRouter;
